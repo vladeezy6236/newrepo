58695b3c43b1f08cd3d622125ad08f7e0af82a61
vladeezy6236 <61150596+vladeezy6236@users.noreply.github.com>
2020-04-11 15:24:42 +0300

    Add files via upload

---
+++ js_basic/basic_task_1/js/main.js
@@ -0,0 +1,8 @@
+let fullname = "Vladislav Cherkasov";
+
+alert(fullname);
+console.log(fullname);
+document.write(fullname);
+
+let element = document.getElementById("box");
+element.append(fullname);
---
+++ js_basic/basic_task_2/js/main.js
@@ -0,0 +1,8 @@
+let fullname = "Vladislav Cherkasov";
+
+document.write(fullname, "<br>");
+
+document.write(5 + 6, "<br>");
+document.write(4 - 5, "<br>");
+document.write(3 * 4 + 2, "<br>");
+document.write(4 / 7, "<br>");
